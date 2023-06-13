8b81eb26dc2e55581e048f0617248efcac2528de
sofcontraining <it@sofcontraining.com>
2023-06-13 11:13:20 +0530

    templates added

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello my first page")
+    # return HttpResponse("Hello my first page")
+    return render(request,'home.html')
 
 def contact(request):
-    return HttpResponse("This is my contact page")
+    # return HttpResponse("This is my contact page")
+    return render(request,'contact.html')
