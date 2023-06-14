658fa1c0907dcc35faf3fb0bb979172bb623fbe9
sofcontraining <it@sofcontraining.com>
2023-06-14 11:15:41 +0530

    static folder added

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('about',views.about),
+    path('services',views.services),
     path('contact',views.contact),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -6,6 +6,14 @@ def home(request):
     # return HttpResponse("Hello my first page")
     return render(request,'home.html')
 
+def about(request):
+    # return HttpResponse("This is my contact page")
+    return render(request,'about.html')
+
+def services(request):
+    # return HttpResponse("This is my contact page")
+    return render(request,'services.html')
+
 def contact(request):
     # return HttpResponse("This is my contact page")
     return render(request,'contact.html')
